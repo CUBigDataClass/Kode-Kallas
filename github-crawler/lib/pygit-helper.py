c9003cb82dffc7ca8353e416bbb2c0d8c7614694
vishwakulkarni <vishwa.kulkarni@gmail.com>
2020-04-11 07:50:45 -0600

    Gitup data crawler

---
+++ github-crawler/lib/pygit-helper.py
@@ -0,0 +1,20 @@
+from github import Github
+
+# using username and password
+#g = Github("user", "password")
+
+# or using an access token
+g = Github("f75b46df7511241ab8481caf80994d4aab7afb68")
+
+org = g.get_organization("CUBigDataClass")
+print(org.login)
+names = org.get_members()
+repos = org.get_repos()
+#
+#for name in names:
+#    print(name)
+
+i=0
+for repo in repos:
+    i=i+1
+    print(i,repo)
---
+++ github-crawler/lib/test.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In order to extract data from Github, we are going to leverage the Github REST API v3, that can be found in this link https://developer.github.com/v3/.
+# In `config.py` file we need to define the following configuration variables, that are going to be accessed by the current notebook:
+# - `GITHUB_USERNAME`
+# - `GITHUB_TOKEN`
+# - `SQL_ALCHEMY_STRING` (only if we want to save our Github results in a relational database)
+
+import json
+import requests
+from pandas.io.json import json_normalize
+from sqlalchemy import create_engine, engine, text, types, MetaData, Table, String
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+import numpy as np
+from datetime import datetime
+
+
+import config
+import os
+
+
+
+# function that converts all object columns to strings, in order to store them efficiently into the database
+def objects_to_strings(table):
+    measurer = np.vectorize(len)
+    df_object = table.select_dtypes(include=[object])
+    string_columns = dict(zip(df_object, measurer(
+        df_object.values.astype(str)).max(axis=0)))
+    string_columns = {key: String(length=value) if value > 0 else String(length=1)
+                      for key, value in string_columns.items() }
+    return string_columns
+
+
+github_api = "https://api.github.com"
+gh_session = requests.Session()
+gh_session.auth = (config.GITHUB_USERNAME, config.GITHUB_TOKEN)
+
+
+def branches_of_repo(repo, owner, api):
+    branches = []
+    next = True
+    i = 1
+    while next == True:
+        url = api + '/repos/{}/{}/branches?page={}&per_page=100'.format(owner, repo, i)
+        branch_pg = gh_session.get(url = url)
+        branch_pg_list = [dict(item, **{'repo_name':'{}'.format(repo)}) for item in branch_pg.json()]    
+        branch_pg_list = [dict(item, **{'owner':'{}'.format(owner)}) for item in branch_pg_list]
+        branches = branches + branch_pg_list
+        if 'Link' in branch_pg.headers:
+            if 'rel="next"' not in branch_pg.headers['Link']:
+                next = False
+        i = i + 1
+    return branches
+
+
+#branches = json_normalize(branches_of_repo('Social-media-impact-cryptocurrency', 'vishwakulkarni', github_api))
+
+
+#branches.to_csv('data/branches.csv')
+
+
+def get_repositories(org_name,owner,api):
+    repos = []
+    next = True
+    i = 1
+    while next == True:
+        url = api + '/orgs/{}/repos??page={}&per_page=100'.format(org_name,i)
+        repos_pg = gh_session.get(url = url)
+        #print(repos_pg.json())
+        repos_pg_list = [dict(item, **{'repo_name':'{}'.format(org_name)}) for item in repos_pg.json()]    
+        repos_pg_list = [dict(item, **{'owner':'{}'.format(owner)}) for item in repos_pg_list]
+        print(repos_pg_list)
+        repos = repos + repos_pg_list
+        if 'Link' in repos_pg.headers:
+            if 'rel="next"' not in repos_pg.headers['Link']:
+                next = False
+        i = i + 1
+        break
+    return repos
+
+orgs = json_normalize(get_repositories('CUBigDataClass', 'vishwakulkarni', github_api))
+
+
+orgs.to_csv('data/org.csv')
