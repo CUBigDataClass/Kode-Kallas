727342076a16bdb01975faa088e9a99d65f688ea
vishwakulkarni <vishwa.kulkarni@gmail.com>
2020-04-11 17:31:47 -0600

    adding comments to elastic search

--- github-crawler/lib/test.py
+++ github-crawler/lib/test.py
@@ -70,7 +70,7 @@ def branches_of_repo(repo, owner, api):
 #branches.to_csv('data/branches.csv')
 
 
-def get_repositories(org_name,owner,api):
+def get_repositories_itterative(org_name,owner,api):
     repos = []
     next = True
     i = 1
@@ -89,7 +89,7 @@ def get_repositories(org_name,owner,api):
         break
     return repos
 #org repo fetcher
-#orgs = json_normalize(get_repositories('CUBigDataClass', 'vishwakulkarni', github_api))
+#orgs = json_normalize(get_repositories_itterative('CUBigDataClass', 'vishwakulkarni', github_api))
 #orgs.to_csv('data/org.csv')
 
 def get_org_information(org_name,owner,api):
@@ -98,13 +98,13 @@ def get_org_information(org_name,owner,api):
     org_data=json.loads(org_data.content)
     return org_data
 
-def send_to_elasticInstance(data):
-    es.index(index='org1', doc_type='_doc', body=data)
+def send_to_elasticInstance(data,index_name,id_val):
+    es.index(index=index_name, doc_type='_doc',id=id_val, body=data)
 
 
 #getting org info
 #org_data = get_org_information('duckduckgo', 'vishwakulkarni', github_api)
-#send_to_elasticInstance(org_data)
+#send_to_elasticInstance(org_data,'org1',org_data['id'])
 
 #how to get documents
 '''
@@ -116,3 +116,56 @@ doc = {
    }
 res = es.search(index='org1', doc_type='_doc', body=doc,scroll='1m')
 print(res)'''
+
+
+#get Repositorries and send to es
+
+def get_repositories(org_name,owner,api):
+    url = api + '/orgs/{}/repos'.format(org_name)
+    org_repos_data = gh_session.get(url = url)
+    org_repos_data=json.loads(org_repos_data.content)
+    return org_repos_data
+
+#uncomment below after testing
+'''
+#get repos and push it to elasticsearch
+org_repos = get_repositories('CUBigDataClass', 'vishwakulkarni',github_api)
+
+for repo in org_repos:
+    repo['license']="test"
+    send_to_elasticInstance(repo,'repos',repo['id'])
+    #commits_of_repo_github()
+
+
+#write repo list to repos_list
+#with open("data/repos_list.json", "w") as outfile: 
+#    outfile.write(json.dumps(org_repos,indent=4)) 
+'''
+
+#save commits of repos as json
+def commits_of_repo_github(repo, owner, api):
+    commits = []
+    next = True
+    i = 1
+    while next == True:
+        url = api + '/repos/{}/{}/commits?page={}&per_page=100'.format(owner, repo, i)
+        commit_pg = gh_session.get(url = url)
+        commit_tp = json.loads(commit_pg.content)
+        for commit in commit_tp:
+            commits.append(commit) 
+        if 'Link' in commit_pg.headers:
+            if 'rel="next"' not in commit_pg.headers['Link']:
+                next = False
+        i = i + 1
+    return commits
+
+commits = commits_of_repo_github('hard-decisions','CUBigDataClass',github_api)
+
+def add_commits_to_elasticsearch(commits):
+    for commit in commits:
+        es.index(index='commits', doc_type='_doc',id=commit['sha'], body=commit)
+
+add_commits_to_elasticsearch(commits)
+
+with open("data/commits.json", "w") as outfile: 
+    outfile.write(json.dumps(commits,indent=4)) 
