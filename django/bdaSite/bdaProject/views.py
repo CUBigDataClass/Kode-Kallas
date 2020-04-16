574e048b6063142a23a94f68604d66b51a49bef1
Siddartha Shankar <siddartha.shankar@Sidds-MacBook-Pro.local>
2020-04-15 22:10:04 -0600

    Test

--- django/bdaSite/bdaProject/views.py
+++ django/bdaSite/bdaProject/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the bdaProject index.")
+
+def trial(request):
+    return HttpResponse("Trial")
