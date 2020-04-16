0ff4d4c00f7836e5f4deced75e558049f1478406
Siddartha Shankar <sish2654@colorado.edu>
2020-04-16 16:55:31 -0600

    UI 4

--- django/bdaSite/bdaProject/views.py
+++ django/bdaSite/bdaProject/views.py
@@ -6,3 +6,10 @@ from django.template import loader
 def index(request):
     context = {}
     return render(request, 'bdaProject/index.html', context)
+
+def org(request, org):
+    
+    context = {
+        'org_name' : org
+    }
+    return render(request, 'bdaProject/organization.html', context)
