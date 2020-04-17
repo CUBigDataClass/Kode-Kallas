03a518ca3b420bc3a17b3c8bc3491612999b1006
shreyas-gopalakrishna <shreyas.g.krishna@gmail.com>
2020-04-16 23:51:34 -0600

    Insert data to Cassandra using CassandraHelper

--- github-analytics/CassandraHelper/CassandraHelper.py
+++ github-analytics/CassandraHelper/CassandraHelper.py
@@ -1,7 +1,18 @@
+import requests
+from CassandraHelper import config as config
+
 class CassandraHelper():
     def __init__(self):
-        self.orgname = ""
+        pass
     
-    def set_org_name(self,orgname):
-        self.orgname = orgname
-        return
+    def insertOrgData(self,orgdata):
+        r = requests.post(config.CASSANDRA_URL_INSERT, json = orgdata)
+        return r.status_code
+
+    def insertUserData(self,userdata):
+        r = requests.post(config.CASSANDRA_URL_INSERT, json = userdata)
+        return r.status_code
+
+    def insertRepoData(self,repodata):
+        r = requests.post(config.CASSANDRA_URL_INSERT, json = repodata)
+        return r.status_code
