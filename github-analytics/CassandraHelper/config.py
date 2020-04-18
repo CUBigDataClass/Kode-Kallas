c01947a1e729d91b68f9323166e3e7ab88d30486
shreyas-gopalakrishna <shreyas.g.krishna@gmail.com>
2020-04-18 13:22:29 -0600

    Updated missing thread count information

--- github-analytics/CassandraHelper/config.py
+++ github-analytics/CassandraHelper/config.py
@@ -9,3 +9,8 @@ CASSANDRA_URL_INSERT = CASSANDRA_URL + "insert/"
 # Github Config
 GITHUB_USER = "shreyas-gopalakrishna"
 GITHUB_TOKEN = "f02831928492a6bfe9f1bca49693f69d1d0e44b3"
+
+
+# Others
+
+THREAD_COUNT = 10
