5e701d8e45b3c1e1780e4a0751b19dfb855e9f7c
Siddartha Shankar <sish2654@colorado.edu>
2020-04-20 01:51:19 -0600

    Removing OAuth Token

--- django/bdaSite/bdaProject/urls.py
+++ django/bdaSite/bdaProject/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('org/<str:org>/', views.org, name='organization'),
-    path('user/<str:userId>/', views.user, name='organization'),
+    path('user/<str:userId>/', views.user, name='user'),
+    path('org/<str:org>/repo/<str:repo>', views.repo, name='repository'),
 ]
