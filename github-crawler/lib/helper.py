ba606c65a2c3dec108441feea1bef45c35b8a7ec
vishwakulkarni <vishwa.kulkarni@gmail.com>
2020-04-21 15:17:31 -0600

    Adding support for ellasandra

--- github-crawler/lib/helper.py
+++ github-crawler/lib/helper.py
@@ -13,11 +13,24 @@ from elasticsearch import Elasticsearch
 
 GITEA_APP_URL = 'YOUR_GITEA_API'
 GITEA_TOKEN = 'a3020c009c6d46783158b5ffb0d1a7c55735bcc4'
-GITHUB_USERNAME = 'karthiks1995'
-GITHUB_TOKEN = '548e0dfe81ed597415acb3047486402b15e181cd'
+GITHUB_USERNAME = 'vishwakulkarni'
+GITHUB_TOKEN = ''
 SQL_ALCHEMY_STRING = ''
 
+import requests
 
+# url = "http://34.66.21.21:5000/insert"
+
+# payload = "{\n    \"org\": \"org\",\n    \"table\": \"repo\",\n    \"body\": [\n        {\n            \"id\": 16750294,\n            \"node_id\": \"MDEwOlJlcG9zaXRvcnkxNjc1MDI5NA==\",\n            \"name\": \"Twitter-EmotiMap\",\n            \"full_name\": \"CUBigDataClass/Twitter-EmotiMap\",\n            \"private\": false,\n            \"owner\": {\n                \"login\": \"CUBigDataClass\",\n                \"id\": 6345918,\n                \"node_id\": \"MDEyOk9yZ2FuaXphdGlvbjYzNDU5MTg=\",\n                \"avatar_url\": \"https://avatars0.githubusercontent.com/u/6345918?v=4\",\n                \"gravatar_id\": \"\",\n                \"url\": \"https://api.github.com/users/CUBigDataClass\",\n                \"html_url\": \"https://github.com/CUBigDataClass\",\n                \"followers_url\": \"https://api.github.com/users/CUBigDataClass/followers\",\n                \"following_url\": \"https://api.github.com/users/CUBigDataClass/following{/other_user}\",\n                \"gists_url\": \"https://api.github.com/users/CUBigDataClass/gists{/gist_id}\",\n                \"starred_url\": \"https://api.github.com/users/CUBigDataClass/starred{/owner}{/repo}\",\n                \"subscriptions_url\": \"https://api.github.com/users/CUBigDataClass/subscriptions\",\n                \"organizations_url\": \"https://api.github.com/users/CUBigDataClass/orgs\",\n                \"repos_url\": \"https://api.github.com/users/CUBigDataClass/repos\",\n                \"events_url\": \"https://api.github.com/users/CUBigDataClass/events{/privacy}\",\n                \"received_events_url\": \"https://api.github.com/users/CUBigDataClass/received_events\",\n                \"type\": \"Organization\",\n                \"site_admin\": false\n            },\n            \"html_url\": \"https://github.com/CUBigDataClass/Twitter-EmotiMap\",\n            \"description\": \"Twitter feed mining with geographical emotional trend analysis.\",\n            \"fork\": false,\n            \"url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap\",\n            \"forks_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/forks\",\n            \"keys_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/keys{/key_id}\",\n            \"collaborators_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/collaborators{/collaborator}\",\n            \"teams_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/teams\",\n            \"hooks_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/hooks\",\n            \"issue_events_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/issues/events{/number}\",\n            \"events_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/events\",\n            \"assignees_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/assignees{/user}\",\n            \"branches_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/branches{/branch}\",\n            \"tags_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/tags\",\n            \"blobs_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/git/blobs{/sha}\",\n            \"git_tags_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/git/tags{/sha}\",\n            \"git_refs_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/git/refs{/sha}\",\n            \"trees_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/git/trees{/sha}\",\n            \"statuses_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/statuses/{sha}\",\n            \"languages_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/languages\",\n            \"stargazers_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/stargazers\",\n            \"contributors_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/contributors\",\n            \"subscribers_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/subscribers\",\n            \"subscription_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/subscription\",\n            \"commits_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/commits{/sha}\",\n            \"git_commits_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/git/commits{/sha}\",\n            \"comments_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/comments{/number}\",\n            \"issue_comment_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/issues/comments{/number}\",\n            \"contents_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/contents/{+path}\",\n            \"compare_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/compare/{base}...{head}\",\n            \"merges_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/merges\",\n            \"archive_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/{archive_format}{/ref}\",\n            \"downloads_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/downloads\",\n            \"issues_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/issues{/number}\",\n            \"pulls_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/pulls{/number}\",\n            \"milestones_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/milestones{/number}\",\n            \"notifications_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/notifications{?since,all,participating}\",\n            \"labels_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/labels{/name}\",\n            \"releases_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/releases{/id}\",\n            \"deployments_url\": \"https://api.github.com/repos/CUBigDataClass/Twitter-EmotiMap/deployments\",\n            \"created_at\": \"2014-02-12T00:05:56Z\",\n            \"updated_at\": \"2016-04-04T16:36:01Z\",\n            \"pushed_at\": \"2014-05-16T14:32:48Z\",\n            \"git_url\": \"git://github.com/CUBigDataClass/Twitter-EmotiMap.git\",\n            \"ssh_url\": \"git@github.com:CUBigDataClass/Twitter-EmotiMap.git\",\n            \"clone_url\": \"https://github.com/CUBigDataClass/Twitter-EmotiMap.git\",\n            \"svn_url\": \"https://github.com/CUBigDataClass/Twitter-EmotiMap\",\n            \"homepage\": \"http://www.mattkgross.com/TwitterMap\",\n            \"size\": 99797,\n            \"stargazers_count\": 0,\n            \"watchers_count\": 0,\n            \"language\": \"Python\",\n            \"has_issues\": true,\n            \"has_projects\": true,\n            \"has_downloads\": true,\n            \"has_wiki\": true,\n            \"has_pages\": false,\n            \"forks_count\": 0,\n            \"mirror_url\": null,\n            \"archived\": false,\n            \"disabled\": false,\n            \"open_issues_count\": 0,\n            \"license\": null,\n            \"forks\": 0,\n            \"open_issues\": 0,\n            \"watchers\": 0,\n            \"default_branch\": \"master\",\n            \"permissions\": {\n                \"admin\": false,\n                \"push\": false,\n                \"pull\": true\n            }\n        }\n    ]\n}"
+# headers = {
+#     'content-type': "application/json",
+#     'cache-control': "no-cache",
+#     'postman-token': "12e57a3e-e251-8cd6-3844-e94e41c04245"
+#     }
+
+# response = requests.request("POST", url, data=payload, headers=headers)
+
+# print(response.text)
 
 class Helper():
     def __init__(self):
@@ -29,6 +42,12 @@ class Helper():
         self.github_api = "https://api.github.com"
         self.gh_session = requests.Session()
         self.gh_session.auth = (GITHUB_USERNAME, GITHUB_TOKEN)
+        self.ellasandra_api = "http://34.66.21.21:5000"
+        self.headers = {
+                    'content-type': "application/json",
+                    'cache-control': "no-cache",
+                    'postman-token': "12e57a3e-e251-8cd6-3844-e94e41c04245"
+                }
     
     def set_org_name(self,orgname):
         self.orgname = orgname
@@ -43,6 +62,42 @@ class Helper():
     def send_to_elasticInstance(self,data,index_name,id_val):
         self.es.index(index=index_name, doc_type='_doc',id=id_val, body=data)
     
+    def send_org_data_to_ellasandra(self,payload):
+        data = {}
+        data['table']='org'
+        data['body'] = []
+        data['body'].append(payload)
+        url = self.ellasandra_api + '/insert'
+        response = requests.request("POST", url, data=json.dumps(data), headers=self.headers)
+        return response.status_code
+    
+    def send_repo_data_to_ellasandra(self,payload):
+        data = {}
+        data['org']=self.orgname
+        data['body'] = payload
+        data['table'] = 'repo'
+        url = self.ellasandra_api + '/insert'
+        response = requests.request("POST", url, data=json.dumps(data), headers=self.headers)
+        return response.status_code
+    
+    def send_users_to_ellasandra(self,payload):
+        data = {}
+        data['org']=self.orgname
+        data['body'] = payload
+        data['table'] = 'users'
+        url = self.ellasandra_api + '/insert'
+        response = requests.request("POST", url, data=json.dumps(data), headers=self.headers)
+        return response.status_code
+    
+    def send_commits_to_ellasandra(self,payload):
+        data = {}
+        data['org']=self.orgname
+        data['body'] = payload
+        data['table'] = 'commits'
+        url = self.ellasandra_api + '/insert'
+        response = requests.request("POST", url, data=json.dumps(data), headers=self.headers)
+        return response.status_code
+
     def get_repositories(self,owner,api):
         repos_list = []
         next = True
@@ -117,8 +172,21 @@ import csv
 #testing comment after use
 h = Helper()
 github_api = "https://api.github.com"
-h.set_org_name("CUBigDataClass")
-print(h.get_org_information("vishwakulkarni",github_api))
+h.set_org_name("games50")
+userInfo = h.get_single_user("https://api.github.com/users/vishwakulkarni","vishwakulkarni",github_api)
+#users = h.get_org_users('CUBigDataClass',github_api)
+test = []
+repo_list = h.get_repositories('vishwakulkarni',github_api)
+for repo in repo_list:
+    repo['license']="test"
+    test.append(repo)
+#print(test)
+data = {}
+data['org']="CUBigDataClass"
+data['table'] = "repo"
+data['body'] = test
+#print(json.dumps(data))
+h.send_to_ellasandra(data)
 
 users = h.get_org_users('CUBigDataClass',github_api)
 ls = {}
